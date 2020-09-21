230ec94269a79c46eb8e764e336a4344fc00d499
Nemanja Lakicevic <n@lakicevic.no>
2020-09-21 14:11:35 +0200

    Working on getTopThreeUsers

--- src/Server/ProfileServant.java
+++ src/Server/ProfileServant.java
@@ -4,6 +4,8 @@ import ProfileService.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class ProfileServant extends ProfilerPOA {
@@ -83,6 +85,45 @@ public class ProfileServant extends ProfilerPOA {
 
     @Override
     public UserCounter[] getTopThreeUsersByRestaurant(String restaurant_id) {
+        String path = "testfiles/test_ordering_profile.txt";
+
+        FileInputStream inputStream = null;
+        Scanner sc = null;
+
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        int lowestCount = 0;
+        try {
+            inputStream = new FileInputStream(path);
+            sc = new Scanner(inputStream, "UTF-8");
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (!line.contains(restaurant_id)) continue;
+                String[] rows = line.split("( |\t)+");
+                int currentCount = Integer.parseInt(rows[3]);
+                if (currentCount > lowestCount) {
+                    map.put(rows[2], currentCount);
+                    currentCount = lowestCount;
+                }
+
+            }
+
+            if (sc.ioException() != null) {
+                throw sc.ioException();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (sc != null) {
+                sc.close();
+            }
+        }
         return new UserCounter[0];
     }
 
