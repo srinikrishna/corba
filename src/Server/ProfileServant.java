e1fb7a3ccd345850186d048cb5740c5df8e46824
Nemanja Lakicevic <n@lakicevic.no>
2020-09-22 17:02:53 +0200

    Working on UserProfile Map and adding orb.db

--- src/Server/ProfileServant.java
+++ src/Server/ProfileServant.java
@@ -11,15 +11,113 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ProfileServant extends ProfilerPOA {
-    private int RESTAURANT_ID_POS = 0;
-    private int FOOD_TYPE_ID_POS = 1;
-    private int USER_ID_POS = 2;
-    private int TIMES_ORDERED_POS = 3;
+    private final int RESTAURANT_ID_POS = 0;
+    private final int FOOD_TYPE_ID_POS = 1;
+    private final int USER_ID_POS = 2;
+    private final int TIMES_ORDERED_POS = 3;
 
-    private int ZONE_POS = 1;
+    private final int ZONE_POS = 1;
 
-    private String orderingPath = "train_in5020/restaurant_ordering_profile.txt";
-    private String zonesPath = "train_in5020/restaurant_location_directory.txt";
+    private final String orderingPath = "train_in5020/restaurant_ordering_profile.txt";
+    private final String zonesPath = "train_in5020/restaurant_location_directory.txt";
+
+    private Map zoneToRestaurantProfilesMap = null;
+
+    public ProfileServant() {
+        init();
+    } //ctor
+
+    private int init() {
+        Map<String, RestaurantProfile> popularRestaurants = new HashMap<>(1000);
+        Map<String, UserProfile> popularUsers = new HashMap<>(1000);
+        Map<String, Map<String, Integer>> popularUserForEachRestaurantMap = new HashMap<>();
+
+        FileInputStream inputStream = null;
+        Scanner sc = null;
+        try {
+            inputStream = new FileInputStream(orderingPath);
+            sc = new Scanner(inputStream, "UTF-8");
+            int minUserTimesOrderedCount = 0;
+
+            // Init popular users section
+            //while (sc.hasNextLine() )
+            int minUserOrders = 0;
+            String minUserOrders_id = null;
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] rows = line.split("( |\t)+");
+
+                String restaurant_id = rows[RESTAURANT_ID_POS];
+                String foodType_id = rows[FOOD_TYPE_ID_POS];
+                String user_id = rows[USER_ID_POS];
+                int timesOrdered = Integer.parseInt(rows[TIMES_ORDERED_POS]);
+
+                if (popularUsers.containsKey(user_id)) {
+                    UserProfile user = popularUsers.get(user_id);
+                    boolean found = false;
+                    for (RestaurantCounter rc : user.restaurants) {
+                        if (restaurant_id.equals(rc.restaurant_id)) {
+                            rc.restaurant_timesOrdered += timesOrdered;
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        RestaurantCounter[] newRc = new RestaurantCounter[user.restaurants.length + 1];
+                        for(int i = 0; i < user.restaurants.length; i++) {
+                            RestaurantCounter oldRc = user.restaurants[i];
+                            newRc[i] = new RestaurantCounter(oldRc.restaurant_id, oldRc.restaurant_timesOrdered);
+                        }
+                        newRc[user.restaurants.length] = new RestaurantCounter(restaurant_id, timesOrdered);
+                        user.restaurants = newRc;
+                    }
+
+                } else if (popularUsers.size() <= 1000 && !popularUsers.containsKey(user_id)) {
+                    RestaurantCounter rc = new RestaurantCounter(restaurant_id, timesOrdered);
+                    UserProfile user = new UserProfile(user_id, new RestaurantCounter[]{rc});
+                    popularUsers.put(user_id, new UserProfile(user_id, new RestaurantCounter[]{rc}));
+                } else if (timesOrdered > minUserOrders ) {
+                    // some code to change the lowest user
+
+                }
+
+                if (!popularUsers.isEmpty()) {
+                    for (UserProfile user : popularUsers.values()) {
+                        int currentUserTimesOrdered = 0;
+                        String id_currentMinOrderUser = null;
+                        for (RestaurantCounter rc : user.restaurants) {
+                            currentUserTimesOrdered += rc.restaurant_timesOrdered;
+                        }
+                        if (currentUserTimesOrdered < minUserOrders) {
+                            minUserOrders = currentUserTimesOrdered;
+                            id_currentMinOrderUser = user.user_id;
+                        }
+                }
+
+                }
+            }
+
+            if (sc.ioException() != null) {
+                throw sc.ioException();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+        return 0;
+    }
 
     @Override
     public int getTimesOrdered(String restaurant_id) {
