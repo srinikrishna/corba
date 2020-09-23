0ff53854870faab44e191ebb96eb6584d8852b43
Nemanja Lakicevic <n@lakicevic.no>
2020-09-23 02:46:26 +0200

    Implemented RestaurantProfile and UserProfile cache. Nees cleanup and some optimization

--- src/Server/ProfileServant.java
+++ src/Server/ProfileServant.java
@@ -2,6 +2,7 @@ package Server;
 
 import ProfileService.*;
 import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import sun.security.util.Cache;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -23,26 +24,180 @@ public class ProfileServant extends ProfilerPOA {
 
     private Map zoneToRestaurantProfilesMap = null;
 
+    private final Map<String, RestaurantProfile> restaurantCache = new HashMap<>(1000);
+    private final Map<String, UserProfile> userCache = new HashMap<>(1000);
+
+
     public ProfileServant() {
         init();
     } //ctor
 
-    private int init() {
-        Map<String, RestaurantProfile> popularRestaurants = new HashMap<>(1000);
-        Map<String, UserProfile> popularUsers = new HashMap<>(1000);
-        Map<String, Map<String, Integer>> popularUserForEachRestaurantMap = new HashMap<>();
+    private static <K, V extends Comparable<? super V>> List<Map.Entry<K, V>> findGreatest(Map<K, V> map, int n)
+    {
+        Comparator<? super Map.Entry<K, V>> comparator = new Comparator<Map.Entry<K, V>>() {
+                    @Override
+                    public int compare(Map.Entry<K, V> e0, Map.Entry<K, V> e1)
+                    {
+                        V v0 = e0.getValue();
+                        V v1 = e1.getValue();
+                        return v0.compareTo(v1);
+                    }
+                };
+        PriorityQueue<Map.Entry<K, V>> highest = new PriorityQueue<Map.Entry<K,V>>(n, comparator);
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            highest.offer(entry);
+            while (highest.size() > n)
+            {
+                highest.poll();
+            }
+        }
 
+        List<Map.Entry<K, V>> result = new ArrayList<Map.Entry<K,V>>();
+        while (highest.size() > 0) {
+            result.add(highest.poll());
+        }
+        return result;
+    }
+
+
+    private int init() {
         FileInputStream inputStream = null;
         Scanner sc = null;
         try {
             inputStream = new FileInputStream(orderingPath);
             sc = new Scanner(inputStream, "UTF-8");
-            int minUserTimesOrderedCount = 0;
+
+            // Init popular restaurants section
+            Map<String, Integer> popRestaurants = new HashMap<>();
+            Map<String, Integer> popUsers = new HashMap<>();
+            long start = System.currentTimeMillis();
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] rows = line.split("( |\t)+");
+                String restaurant_id = rows[RESTAURANT_ID_POS];
+                String user_id = rows[USER_ID_POS];
+                int count = Integer.parseInt(rows[TIMES_ORDERED_POS]);
+
+                if (popRestaurants.computeIfPresent(restaurant_id, (k, v) -> v += count) == null) {
+                    popRestaurants.put(restaurant_id, count);
+                }
+                if (popUsers.computeIfPresent(user_id, (k, v) -> v += count) == null) {
+                    popUsers.put(user_id, count);
+                }
+            }
+            long end = System.currentTimeMillis();
+            long lapsed1 = end - start;
+            System.out.println(lapsed1);
+            Map<String, Integer> sortedRestaurants = findGreatest(popRestaurants, 1000).stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            Map<String, Integer> sortedUsers = findGreatest(popUsers, 1000).stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+
+            long lapsed2 = System.currentTimeMillis() - end;
+            System.out.println(lapsed2);
+
+            inputStream = new FileInputStream(orderingPath);
+            sc = new Scanner(inputStream, "UTF-8");
+
+            long start2 = System.currentTimeMillis();
+            while(sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] rows = line.split("( |\t)+");
+                String restaurant_id = rows[RESTAURANT_ID_POS];
+                String user_id = rows[USER_ID_POS];
+                String foodType_id = rows[FOOD_TYPE_ID_POS];
+                int count = Integer.parseInt(rows[TIMES_ORDERED_POS]);
+
+                if (restaurantCache.containsKey(restaurant_id)) {
+                    RestaurantProfile rp = restaurantCache.get(restaurant_id);
+                    boolean foodTypeFound = false;
+                    for (FoodTypeCounter ftc : rp.foodTypes) {
+                        if (ftc != null && ftc.foodType_id.equals(foodType_id)) {
+                            ftc.foodType_timesOrdered += count;
+                            foodTypeFound = true;
+                            break;
+                        }
+                    }
+                    if (!foodTypeFound) {
+                        if (rp.foodTypes[1] == null) {
+                            rp.foodTypes[1] = new FoodTypeCounter(foodType_id, count);
+                            sortFirstPairFoodTypeCounters(rp.foodTypes);
+                        } else if (rp.foodTypes[2] == null) {
+                            rp.foodTypes[2] = new FoodTypeCounter(foodType_id, count);
+                            sortAllFoodTypeCounters(rp.foodTypes);
+                        } else if (rp.foodTypes[0].foodType_timesOrdered < count) {
+                            rp.foodTypes[0] = new FoodTypeCounter(foodType_id, count);
+                            sortAllFoodTypeCounters(rp.foodTypes);
+                        }
+                    }
+
+                    boolean userCounterFound = false;
+                    for (UserCounter uc : rp.top_three_users) {
+                        if (uc != null && uc.user_id.equals(user_id)) {
+                            uc.restaurant_timesOrdered += count;
+                            userCounterFound = true;
+                            break;
+                        }
+                    }
+
+                    if (!userCounterFound) {
+                        if (rp.top_three_users[1] == null) {
+                            rp.top_three_users[1] = new UserCounter(foodType_id, count);
+                            sortFirstPairUserCounters(rp.top_three_users);
+                        } else if (rp.top_three_users[2] == null) {
+                            rp.top_three_users[2] = new UserCounter(foodType_id, count);
+                            sortAllUserCounters(rp.top_three_users);
+                        } else if (rp.top_three_users[0].restaurant_timesOrdered < count) {
+                            rp.top_three_users[0] = new UserCounter(foodType_id, count);
+                            sortAllUserCounters(rp.top_three_users);
+                        }
+                    }
+                    //restaurantCache.remove(restaurant_id, rp);
+                } else if (sortedRestaurants.containsKey(restaurant_id)) {
+                    RestaurantProfile rp = new RestaurantProfile();
+                    rp.restaurant_id = restaurant_id;
+                    rp.total_times_ordered = sortedRestaurants.get(restaurant_id);
+                    rp.foodTypes = new FoodTypeCounter[3];
+                    rp.foodTypes[0] = new FoodTypeCounter(foodType_id, count);
+                    rp.top_three_users = new UserCounter[3];
+                    rp.top_three_users[0] = new UserCounter(user_id, count);
+                    restaurantCache.put(restaurant_id, rp);
+                    //System.out.println("check if replaced");
+                }
+
+                if (userCache.containsKey(user_id)) {
+                    UserProfile up = userCache.get(user_id);
+                    boolean restaurantFound = false;
+                    for (RestaurantCounter rc : up.restaurants) {
+                        if (rc.restaurant_id.equals(restaurant_id)) {
+                            rc.restaurant_timesOrdered += count;
+                            restaurantFound = true;
+                            break;
+                        }
+                    }
+                    if (!restaurantFound) {
+                        RestaurantCounter rc = new RestaurantCounter(restaurant_id, count);
+                        // Make a new RC array with size of the previous + 1 for the new element
+                        RestaurantCounter[] rcArray = new RestaurantCounter[up.restaurants.length + 1];
+                        // Copy all restaurants into the new array
+                        System.arraycopy(up.restaurants, 0, rcArray, 0, up.restaurants.length);
+                        // Populate the final element
+                        rcArray[up.restaurants.length] = new RestaurantCounter(restaurant_id, count);
+                        // Replace
+                        up.restaurants = rcArray;
+                    }
+                } else if (sortedUsers.containsKey(user_id)) {
+                    RestaurantCounter rc = new RestaurantCounter(restaurant_id, count);
+                    UserProfile up = new UserProfile(user_id, new RestaurantCounter[]{rc});
+                    userCache.put(user_id, up);
+                }
+            }
+            long lapsed3 = System.currentTimeMillis() - start2;
+            System.out.println(lapsed3);
 
             // Init popular users section
-            //while (sc.hasNextLine() )
-            int minUserOrders = 0;
-            String minUserOrders_id = null;
+/*
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] rows = line.split("( |\t)+");
@@ -62,7 +217,6 @@ public class ProfileServant extends ProfilerPOA {
                             break;
                         }
                     }
-
                     if (!found) {
                         RestaurantCounter[] newRc = new RestaurantCounter[user.restaurants.length + 1];
                         for(int i = 0; i < user.restaurants.length; i++) {
@@ -73,30 +227,38 @@ public class ProfileServant extends ProfilerPOA {
                         user.restaurants = newRc;
                     }
 
-                } else if (popularUsers.size() <= 1000 && !popularUsers.containsKey(user_id)) {
+                } else if (popularUsers.size() < 1000 && !popularUsers.containsKey(user_id)) {
                     RestaurantCounter rc = new RestaurantCounter(restaurant_id, timesOrdered);
                     UserProfile user = new UserProfile(user_id, new RestaurantCounter[]{rc});
                     popularUsers.put(user_id, new UserProfile(user_id, new RestaurantCounter[]{rc}));
-                } else if (timesOrdered > minUserOrders ) {
+
+                    if (popularUsers.size() == 1000) {
+                        int currUserTimesOrdered = 0;
+                        int currMinTimeOrdered = 4000000;
+                        String currMinTimesOrderedUser_id = null;
+                        for (UserProfile up : popularUsers.values()) {
+                            for (RestaurantCounter rCounter : up.restaurants) {
+                                currUserTimesOrdered += rCounter.restaurant_timesOrdered;
+                            }
+
+                            if (currUserTimesOrdered < currMinTimeOrdered) {
+                                currMinTimeOrdered = currUserTimesOrdered;
+                            }
+                        }
+                    }
+
+                } else if (timesOrdered > minUserTimesOrderedCount ) {
                     // some code to change the lowest user
 
                 }
 
-                if (!popularUsers.isEmpty()) {
-                    for (UserProfile user : popularUsers.values()) {
-                        int currentUserTimesOrdered = 0;
-                        String id_currentMinOrderUser = null;
-                        for (RestaurantCounter rc : user.restaurants) {
-                            currentUserTimesOrdered += rc.restaurant_timesOrdered;
-                        }
-                        if (currentUserTimesOrdered < minUserOrders) {
-                            minUserOrders = currentUserTimesOrdered;
-                            id_currentMinOrderUser = user.user_id;
-                        }
-                }
-
+                if (popularUsers.size() == 1000 && timesOrdered > minUserTimesOrderedCount) {
+                    assert minUserOrders_id != null;
+                    popularUsers.remove(minUserOrders_id);
+                    // Add new object
                 }
             }
+ */
 
             if (sc.ioException() != null) {
                 throw sc.ioException();
@@ -117,6 +279,54 @@ public class ProfileServant extends ProfilerPOA {
         }
 
         return 0;
+    }
+
+    private static void sortFirstPairFoodTypeCounters(FoodTypeCounter[] ftc) {
+        FoodTypeCounter temp = null;
+        if (ftc[0].foodType_timesOrdered > ftc[1].foodType_timesOrdered) {
+            temp = ftc[0];
+            ftc[0] = ftc[1];
+            ftc[1] = temp;
+        }
+    }
+
+    private static void sortSecondPairFoodTypeCounters(FoodTypeCounter[] ftc) {
+        FoodTypeCounter temp = null;
+        if (ftc[1].foodType_timesOrdered > ftc[2].foodType_timesOrdered) {
+            temp = ftc[1];
+            ftc[1] = ftc[2];
+            ftc[2] = temp;
+        }
+    }
+
+    private static void sortAllFoodTypeCounters(FoodTypeCounter[] ftc) {
+        sortFirstPairFoodTypeCounters(ftc);
+        sortSecondPairFoodTypeCounters(ftc);
+        sortFirstPairFoodTypeCounters(ftc);
+    }
+
+    private static void sortFirstPairUserCounters(UserCounter[] uc) {
+        UserCounter temp = null;
+        if (uc[0].restaurant_timesOrdered > uc[1].restaurant_timesOrdered) {
+            temp = uc[0];
+            uc[0] = uc[1];
+            uc[1] = temp;
+        }
+    }
+
+    private static void sortSecondPairUserCounters(UserCounter[] uc) {
+        UserCounter temp = null;
+        if (uc[1].restaurant_timesOrdered > uc[2].restaurant_timesOrdered) {
+            temp = uc[1];
+            uc[1] = uc[2];
+            uc[2] = temp;
+        }
+    }
+
+    private static void sortAllUserCounters(UserCounter[] uc) {
+        sortFirstPairUserCounters(uc);
+        sortSecondPairUserCounters(uc);
+        sortFirstPairUserCounters(uc);
     }
 
     @Override
